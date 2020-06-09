be8296fe14c881090c8b59d307b547467c7f3de4
hmisonne <helene.misonne@gmail.com>
2020-06-09 11:54:03 -0700

    Added Form and Radio buttons

--- client/src/components/QuestionDetails.tsx
+++ client/src/components/QuestionDetails.tsx
@@ -1,14 +1,9 @@
 import * as React from 'react'
 import { History } from 'history'
 import {
-  Divider,
-  Grid,
   Header,
-  Loader,
-  Button,
-  Icon
+  Form,
 } from 'semantic-ui-react'
-
 import Auth from '../auth/Auth'
 import { Question } from '../types/Question'
 
@@ -16,23 +11,52 @@ import { Question } from '../types/Question'
 interface QuestionsProps {
   auth: Auth
   history: History
+  match: {
+    params: {
+      questionId: string
+    }
+  }
 }
 
 interface QuestionsState {
-  questions: Question[]
-  loadingQuestions: boolean
+  value: string
+  optionOneText: string
+  optionTwoText: string
 }
+
 
 export class QuestionDetails extends React.PureComponent<QuestionsProps, QuestionsState> {
   state: QuestionsState = {
-    questions: [],
-    loadingQuestions: true
+    value: 'optionOneText',
+    optionOneText: 'Do that',
+    optionTwoText: 'Do this'
   }
+  handleChange = (e: any, { value }:any) => this.setState({ value })
 
   render() {
+    const { value } = this.state
+    const {optionOneText, optionTwoText} = this.state
     return (
       <div>
         <Header as="h1">Would You Rather</Header>
+        <Form>
+          <Form.Group widths='equal'>
+
+            <Form.Radio
+              label={optionOneText}
+              value='optionOneText'
+              checked={value === 'optionOneText'}
+              onChange={this.handleChange}
+              />
+              <Form.Radio
+              label={optionTwoText}
+              value='optionTwoText'
+              checked={value === 'optionTwoText'}
+              onChange={this.handleChange}
+              />
+              <Form.Button>Vote</Form.Button>
+          </Form.Group>
+        </Form>
       </div>
     )
   }
