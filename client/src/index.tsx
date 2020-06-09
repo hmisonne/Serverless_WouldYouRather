2c29c0abeb88291098026967396814318b701e26
hmisonne <helene.misonne@gmail.com>
2020-06-09 11:53:28 -0700

    Added import semantic ui to add styles

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { makeAuthRouting } from './routing';
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render( makeAuthRouting(),
   document.getElementById('root')
