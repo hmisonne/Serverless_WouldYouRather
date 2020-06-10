8387421b7f2f315aefb00d00d42c300b6c2b7c5d
hmisonne <helene.misonne@gmail.com>
2020-06-10 12:18:38 -0700

    Added logger and updated expiration to number

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -5,10 +5,12 @@ import { updateQuestionUrl } from '../../businessLogic/questions'
 import { getUserId } from '../utils'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
+import { createLogger } from '../../utils/logger'
 
+const logger = createLogger('uploadImage')
 
 const s3 = new AWS.S3({
-  signatureVersion: 'v4'
+    signatureVersion: 'v4'
 })
 
 const bucketName = process.env.ATTACHEMENTS_S3_BUCKET
@@ -16,35 +18,36 @@ const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const questionId = event.pathParameters.questionId
+    logger.info('Processing event: ', event)
+    const questionId = event.pathParameters.questionId
 
-  // Update dynamoDb with Url
-  const uploadUrl = getUploadUrl(questionId)
-  const userId = getUserId(event)
-  const updatedQuestion = {
-    attachmentUrl: `https://${bucketName}.s3.amazonaws.com/${questionId}`
-  }
+    // Update dynamoDb with Url
+    const uploadUrl = getUploadUrl(questionId)
+    const userId = getUserId(event)
+    const updatedQuestion = {
+        attachmentUrl: `https://${bucketName}.s3.amazonaws.com/${questionId}`
+    }
 
-  await updateQuestionUrl(updatedQuestion, userId, questionId)
-  
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      uploadUrl
-    })
-  }
+    await updateQuestionUrl(updatedQuestion, userId, questionId)
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            uploadUrl
+        })
+    }
 })
 
 function getUploadUrl(questionId: string) {
-  return s3.getSignedUrl('putObject', {
-    Bucket: bucketName,
-    Key: questionId,
-    Expires: urlExpiration
-  })
+    return s3.getSignedUrl('putObject', {
+        Bucket: bucketName,
+        Key: questionId,
+        Expires: +urlExpiration
+    })
 }
 
 handler.use(
-  cors({
-    credentials: true
-  })
+    cors({
+        credentials: true
+    })
 )
