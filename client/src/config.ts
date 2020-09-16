99470549a44ef795564566625e9deeb2f4067c09
hmisonne <helene.misonne@gmail.com>
2020-09-16 09:22:44 -0700

    fix: Updated callbackUrl and ClientId

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = 'xfiw724dof'
+const apiId = 'hqats19rc8'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   domain: 'fsnd-hm.auth0.com',            // Auth0 domain
-  clientId: 'NeVPTKmxhvQSaAyFQuOGfsjNutXBDDTg',          // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  clientId: 'vfgU0m5O47Me30hRP33IZ2qJ3AAPWaRm',          // Auth0 client id
+  callbackUrl: 'http://wyr-frontend-dev.s3-website-us-east-1.amazonaws.com/callback'
 }
