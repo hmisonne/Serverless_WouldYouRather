c68d11dd969a5f622dfeb6e069928c4b98437d2e
hmisonne <helene.misonne@gmail.com>
2020-09-28 11:08:41 -0700

    fix: Update callbackUrl to match with new domain name

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,6 +3,9 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   domain: 'fsnd-hm.auth0.com',            // Auth0 domain
-  clientId: '1utMzxRA3Wb0CX3tXqQ1aEN4nWCrxx61',          // Auth0 client id
-  callbackUrl: 'http://wyr-frontend-dev.s3-website-us-east-1.amazonaws.com/callback'
+  clientId: 'vfgU0m5O47Me30hRP33IZ2qJ3AAPWaRm',          // Auth0 client id
+  callbackUrl: 'https://hmisonne.com/callback'
 }
+
+// callbackUrl: 'http://wyr-frontend-dev.s3-website-us-east-1.amazonaws.com/callback'
+// callbackUrl: 'http://localhost:3000/callback'
